441668282debeb173c562a2949eb97e84626d1a7
angelofabricante202400636-wq <angelofabricante.202400636@gmail.com>
2026-04-10 09:09:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# FABRICANTE-ANGELO-Y.
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/angelofabricante202400636-wq/FABRICANTE-ANGELO-Y.)
---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+//serve HTML file in back end
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Dataset
+const students = [
+  {
+    id: 1,
+    name: 'nigger1 ',
+    yearLevel: 1,
+  },
+  {
+    id: 2,
+    name: 'nigger2 ',
+    yearLevel: 2,
+  },
+  {
+    id: 3,
+    name: 'nigger3 ',
+    yearLevel: 3,
+  },
+];
+
+//API endpoint
+app.get('/api/students', (req, res) => {
+  res.json(students);
+});
+
+//Listen for incoming request
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
